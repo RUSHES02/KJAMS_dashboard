c76b4e2cd302619a8fd6279639a7951b7f97a1c3
Abhi <abhijaanganuly02@gmail.com>
2023-06-24 12:54:51 +0530

    initial setup 2

--- KJAMS/src/App.jsx
+++ KJAMS/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GridSystem from './Components/GridSystem'
+import Buttons from './Components/Buttons'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +23,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Buttons />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- KJAMS/src/main.jsx
+++ KJAMS/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/scss/paper-dashboard.scss?v=1.3.1";
+import "./assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
