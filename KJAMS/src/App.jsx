66b0051ccc0ba78bf050df3800ce8c2feadfdf67
Abhi <abhijaanganuly02@gmail.com>
2023-06-26 19:32:01 +0530

    faculy react table created

--- KJAMS/src/App.jsx
+++ KJAMS/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
+      <div className='Center'>
         <Faculty />
       </div>
   )
--- KJAMS/src/Pages/Faculty.jsx
+++ KJAMS/src/Pages/Faculty.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from '/src/assets/react.svg'
 import viteLogo from '/vite.svg'
 import '/src/App.css'
+import Select from "react-select";
 
 import ReactTables from '/src/Components/ReactTables'
 import ValidationForms from '../Components/ValidationForms'
 import RegularForms from '../Components/RegularForms'
 import ExtendedForms from '../Components/ExtendedForms'
 import AdminNavbar from '../Components/AdminNavbar'
+import Buttons from '../Components/Buttons'
 
 import {
     Button,
@@ -30,6 +32,17 @@ function Faculty() {
     const [count, setCount] = useState(0)
     const [loginFullNameState, setloginFullNameState] = useState("");
     const [loginFullName, setloginFullName] = useState("");
+    const [loginEmailState, setloginEmailState] = useState("");
+    const [loginPasswordState, setloginPasswordState] = useState("");
+    const [departmentSelect, handleDepartmentSelect] = useState({
+      value: 0,
+      label: "Department",
+    });
+
+    const [clubSelect, handleClubSelect] = useState({
+      value: 0,
+      label: "Club",
+    });
 
     const verifyLength = (value, length) => {
         if (value.length >= length) {
@@ -50,12 +63,26 @@ function Faculty() {
         }
       };
 
+      const verifyEmail = (value) => {
+        var emailRex =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (emailRex.test(value)) {
+          return true;
+        }
+        return false;
+      };
+
     return (
         <div >
-            {/* <Row> */}
-                <ExtendedTables />
-                {/* <AdminNavbar /> */}
-                {/* <FormGroup className={`has-label ${loginFullNameState}`}>
+            <Row>
+            <Card style={{minWidth: "1366px"}}>
+              <CardHeader>
+                <CardTitle tag="h3" className='text-left'>Add Faculty</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Row>
+                  <Col>
+                  <FormGroup className={`has-label ${loginFullNameState}`}>
                     <label>Full Name *</label>
                     <Input
                       name="fullname"
@@ -72,10 +99,91 @@ function Faculty() {
                     {loginFullNameState === "has-danger" ? (
                       <label className="error">This field is required.</label>
                     ) : null}
+                    </FormGroup>
+                  </Col>
+                  <Col>
+                  <FormGroup className={`has-label ${loginEmailState}`}>
+                    <label>Email Address *</label>
+                    <Input
+                      name="email"
+                      type="email"
+                      onChange={(e) => {
+                        if (!verifyEmail(e.target.value)) {
+                          setloginEmailState("has-danger");
+                        } else {
+                          setloginEmailState("has-success");
+                        }
+                        setloginEmail(e.target.value);
+                      }}
+                    />
+                    {loginEmailState === "has-danger" ? (
+                      <label className="error">
+                        Please enter a valid email address.
+                      </label>
+                    ) : null}
                   </FormGroup>
-                  </Row> */}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                  <Select
+                      className="react-select primary"
+                      classNamePrefix="react-select"
+                      name="departmentSelect"
+                      value={departmentSelect}
+                      onChange={(value) => {
+                        handleDepartmentSelect(value);
+                      }}
+                      options={[
+                        {
+                          value: "",
+                          label: "Department",
+                          isDisabled: true
+                        },
+                        { value: "2", label: "Computer Science(UG)" },
+                        { value: "3", label: "BBA" }
+                      ]}
+                      placeholder="Department"
+                    />
+                  </Col>
+                  <Col>
+                  <Select
+                    className="react-select primary"
+                    classNamePrefix="react-select"
+                    name="clubSelect"
+                    value={clubSelect}
+                    onChange={(value) => {
+                      handleClubSelect(value);
+                    }}
+                    options={[
+                      {
+                        value: "",
+                        label: "None",
+                        isDisabled: false
+                      },
+                      { value: "2", label: "LCA" },
+                      { value: "3", label: "Debating Society" },
+                      { value: "4", label: "UBA" }
+                    ]}
+                    placeholder="Club"
+                  />
+                  </Col>
+                </Row>
+            
+                <Button color="primary" className="col-12 my-4 mx-auto primaryColor">ADD</Button>
+                </CardBody>
+              </Card>
+            </Row>
+
+            <Row>
+                <ReactTables />   
+            </Row>
+            <Buttons />
         </div>
     )
   }
   
   export default Faculty
+
+
+  
